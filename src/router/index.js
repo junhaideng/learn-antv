fc6da999193b07f12b963aac4b9ed4c112389949
Cyberist-Edgar <201648748@qq.com>
2020-05-16 20:42:01 +0800

    add router

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import VueRouter from 'vue-router';
+
+export default new VueRouter({
+    mode: "history",
+    routes: [{
+            path: "/",
+            redirect: "/chart"
+        },
+        {
+            path: "/chart",
+            name: "chart",
+            component: () => import("../components/common/Base"),
+            children: [
+                {path: "/chart",
+                    name: "home",
+            component: () => import("../components/home/index")
+                },{
+                    path: "interval",
+                    name: "interval",
+                    component: () => import("../components/charts/Interval")
+                },
+                {
+                    path: "point",
+                    name: "point",
+                    component: () => import("../components/charts/Point")
+                }
+            ]
+        },
+
+    ]
+
+})
