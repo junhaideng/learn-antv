58b3f69fd4b070c8ffa270cc3e8f3c2acc4fe59c
Cyberist-Edgar <201648748@qq.com>
2020-05-17 09:43:31 +0800

    add new api

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,6 +1,24 @@
 const Mock = require('mockjs')
 
-Mock.mock("/api/get_column_data", "get", function () {
+// 柱形图需要的数据
+Mock.mock("/api/get_interval_data", "get", function() {
+    return Mock.mock({
+        data:[
+            { type: '未知', value: 654, percent: 0.02 },
+            { type: '17 岁以下', value: 654, percent: 0.02 },
+            { type: '18-24 岁', value: 4400, percent: 0.2 },
+            { type: '25-29 岁', value: 5300, percent: 0.24 },
+            { type: '30-39 岁', value: 6200, percent: 0.28 },
+            { type: '40-49 岁', value: 3300, percent: 0.14 },
+            { type: '50 岁以上', value: 1500, percent: 0.06 },
+          ]
+    })
+})
+
+
+
+// 散点图需要的数据
+Mock.mock("/api/get_point_data", "get", function () {
     return Mock.mock({
         data: [{
                 feature: 'Battery',
